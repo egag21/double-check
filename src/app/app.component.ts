ad0b8bc779298d03e7c126fb77b531b65d91b895
jgage@gemedot.com <jgage@gemedot.com>
2024-06-28 16:26:59 +0200

    minor formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 // file: app.component.ts
 
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { BudgetItem, addItem } from './state/budget.actions';
 import { BudgetState } from './state/budget.reducer';
@@ -10,7 +10,7 @@ import { BudgetState } from './state/budget.reducer';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   @ViewChild('dateSelect') dateSelect!: ElementRef<HTMLSelectElement>;
 
   months: string[] = [
@@ -19,8 +19,13 @@ export class AppComponent {
   ];
   dates: string[] = ['June 24'];
   selectedDate: string = this.dates[0];
+  dropdownWidth: string = 'auto';
 
   constructor(private store: Store<{ budget: BudgetState }>) { }
+
+  ngAfterViewInit() {
+    this.updateDropdownWidth();
+  }
 
   onAddItem(item: BudgetItem) {
     this.store.dispatch(addItem({ item }));
@@ -42,9 +47,20 @@ export class AppComponent {
     this.dates.push(newDate);
     this.selectedDate = newDate;
 
+    this.updateDropdownWidth();
+
     // Set focus to the select element
     setTimeout(() => {
       this.dateSelect.nativeElement.focus();
+    }, 0);
+  }
+
+  updateDropdownWidth() {
+    setTimeout(() => {
+      const selectElement = this.dateSelect.nativeElement;
+      selectElement.style.width = 'auto'; // Reset width to auto before calculating
+      const width = selectElement.scrollWidth;
+      this.dropdownWidth = `${width + 10}px`; // Add some padding to avoid clipping
     }, 0);
   }
 }
